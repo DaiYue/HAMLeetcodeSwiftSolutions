781bafc9e227e75c0113ff19169aa81c14154e06
DaiYue <daiyue10@gmail.com>
2016-12-06 17:44:18 +0800

    #2 Add Two Numbers

---
+++ solutions/2_Add_Two_Numbers.playground/Contents.swift
@@ -0,0 +1,77 @@
+// #2 Add Two Numbers https://leetcode.com/problems/add-two-numbers/
+// 简单的单链表处理。考虑几种情况：1. 两个数位数相等，且最高位不需进位 2. 两个数位数相等，且最高位需要进位 3. 两个数位数不相等
+// 时间复杂度：O(n) 空间复杂度：O(n)
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        if l1 == nil {
+            return l2
+        }
+        
+        if l2 == nil {
+            return l1
+        }
+        
+        var l1Tail:ListNode? = l1
+        var l2Tail:ListNode? = l2
+        
+        var head:ListNode? = nil
+        var tail:ListNode? = nil
+
+        var carry:Int = 0
+        while l1Tail != nil || l2Tail != nil {
+            var val = (l1Tail?.val ?? 0) + (l2Tail?.val ?? 0) + carry
+            if val >= 10 {
+                carry = 1
+                val -= 10
+            } else {
+                carry = 0
+            }
+            
+            let node = ListNode(val)
+            
+            if head == nil {
+                head = node
+                tail = node
+            } else {
+                tail!.next = node
+                tail = node
+            }
+            
+            l1Tail = l1Tail?.next
+            l2Tail = l2Tail?.next
+        }
+        
+        if carry != 0 {
+            let node = ListNode(carry)
+            tail?.next = node
+        }
+        
+        return head;
+    }
+}
+
+let n1 = ListNode(2)
+let n2 = ListNode(4)
+n1.next = n2
+let n3 = ListNode(9)
+n2.next = n3
+
+let n4 = ListNode(5)
+let n5 = ListNode(6)
+n4.next = n5
+let n6 = ListNode(4)
+n5.next = n6
+
+let solution = Solution();
+let nr = solution.addTwoNumbers(n1, n4)
+
