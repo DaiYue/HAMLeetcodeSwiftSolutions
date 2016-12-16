f1596ed8813d9d9c0ff02753dbd4db3b5e1feb0c
DaiYue <daiyue10@gmail.com>
2016-12-16 12:02:22 +0800

    #8 String to Integer

--- solutions/7_Reverse_Integer.playground/Contents.swift
+++ solutions/7_Reverse_Integer.playground/Contents.swift
@@ -1,5 +1,6 @@
 // #7 Reverse Integer https://leetcode.com/problems/reverse-integer/
 // 这题本身很简单，但感觉是道不错的面试题。可以测试面试者是否考虑各种边界情况，对溢出有没有概念。
+// 测试用例对 swift 给的不对，只能用 Int32.max。我是把负数统一归成正数来计算，这样判断溢出的语句可以简单点。
 
 class Solution {
     func reverse(_ x: Int) -> Int {
---
+++ solutions/8_String_to_Integer.playground/Contents.swift
@@ -0,0 +1,52 @@
+// #8 String to Integer https://leetcode.com/problems/string-to-integer-atoi/
+
+class Solution {
+    func myAtoi(_ str: String) -> Int {
+        let chars = [Character](str.characters)
+        
+        var result = 0
+        var sign = 0
+        var isPrefix = true
+        for char in chars {
+            if (char == " " || char == " ") && isPrefix {
+                continue
+            }
+            
+            isPrefix = false
+            
+            if char == "+" || char == "-" {
+                if sign != 0 {
+                    break
+                }
+                sign = char == "+" ? 1 : -1
+                continue
+            }
+            
+            if char >= "0" && char <= "9" {
+                let charValue = Int(String(char))!
+                
+                if sign >= 0 && result > (Int(Int32.max) - charValue) / 10 {
+                    result = Int(Int32.max)
+                    break
+                }
+                
+                if sign == -1 && result > (-Int(Int32.min) - charValue) / 10 {
+                    result = -Int(Int32.min)
+                    break
+                }
+                
+                result = result * 10 + charValue
+                
+            } else {
+                break
+            }
+        }
+        
+        if sign == 0 {
+            sign = 1
+        }
+        return result * sign
+    }
+}
+
+Solution().myAtoi("9223372036854775809")
