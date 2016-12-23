51de90033a9aa23d1a0cd08453a19978c2e66b80
DaiYue <daiyue10@gmail.com>
2016-12-23 15:12:02 +0800

    #10 Regular Expression Matching
    Refine code.

--- solutions/10_Regular_Expression_Matching.playground/Contents.swift
+++ solutions/10_Regular_Expression_Matching.playground/Contents.swift
@@ -1,4 +1,5 @@
 // #10 Regular Expression Matching https://leetcode.com/problems/regular-expression-matching/
+//
 
 struct Token {
     var char:Character
@@ -7,103 +8,75 @@ struct Token {
 
 class Solution {
     func isMatch(_ s: String, _ p: String) -> Bool {
-        let sChars = [Character](s.characters)
-        let tokens = generateTokensFrom(pattern: p)
-        
-        return isMatch(sChars: sChars, tokens: tokens)
+        return isMatch(sChars: [Character](s.characters), tokens: generateTokensFrom(pattern: p))
     }
     
     func generateTokensFrom(pattern:String) -> [Token] {
         let chars = [Character](pattern.characters)
-        
-        guard chars.count > 0 else {
-            return []
-        }
-        
         var tokens = [Token]()
-        var lastToken:Token? = nil
         var index = 0
-        while index <= chars.count - 2 {
+        while index < chars.count {
             let char = chars[index]
-            let nextChar = chars[index + 1]
+            let nextChar = index + 1 < chars.count ? chars[index + 1] : " "
             
             if nextChar == "*" {
-                if lastToken != nil && lastToken?.isStar == true {
-                    if lastToken!.char == char || lastToken!.char == "." {
-                        // merge
-                        index = index + 2;
-                        continue
-                    }
-                    if char == "." {
-                        while lastToken?.isStar == true {
-                            // discard
-                            lastToken = tokens.popLast()
-                        }
-                        if lastToken != nil && lastToken?.isStar == false {
-                            tokens.append(lastToken!)
-                        }
+                index = index + 2
+                let lastToken = tokens.count > 0 ? tokens.last! : Token(char: " ", isStar: false)
+                if lastToken.isStar == true && (lastToken.char == char || lastToken.char == ".") {
+                    continue // ignore x* after x* or .*
+                }
+                if char == "." {
+                    while tokens.count > 0 && tokens.last?.isStar == true {
+                        tokens.popLast() // remove every x* before .*
                     }
                 }
-                lastToken = Token(char: char, isStar: true)
-                index = index + 2
-                
+                tokens.append(Token(char: char, isStar: true))
             } else {
-                lastToken = Token(char: char, isStar: false)
                 index = index + 1
+                tokens.append(Token(char: char, isStar: false))
             }
-            tokens.append(lastToken!)
-        }
-        if index <= chars.count - 1 {
-            tokens.append(Token(char: chars[index], isStar: false))
         }
         
         return tokens
     }
     
     func isMatch(sChars : [Character], tokens : [Token]) -> Bool {
-        
         guard tokens.count > 0 else {
-            if sChars.count == 0 {
-                return true
+            return sChars.count == 0 ? true : false
+        }
+        
+        guard sChars.count > 0 else {
+            for token in tokens {
+                if !token.isStar {
+                    return false
+                }
             }
-            return false
+            return true
         }
         
         let token = tokens[0]
-        let sChar = sChars.count > 0 ? sChars[0] : nil
-        let nextS = dropFirstSafe(array: sChars) as! [Character]
-        let nextTokens = dropFirstSafe(array: tokens) as! [Token]
+        let sChar = sChars[0]
+        let nextS = Array(sChars.dropFirst())
+        let nextTokens = Array(tokens.dropFirst())
         
-        let match = (token.char == "." && sChar != nil) || token.char == sChar
-        
+        let match = token.char == "." || token.char == sChar
         if token.isStar {
             if match && (isMatch(sChars:nextS, tokens:nextTokens) || isMatch(sChars:nextS, tokens:tokens)) {
                 return true
             }
             return isMatch(sChars:sChars, tokens:nextTokens)
-            
         } else {
-            if match {
-                return isMatch(sChars:nextS, tokens:nextTokens)
-            }
-            return false
+            return match && isMatch(sChars:nextS, tokens:nextTokens)
         }
-    }
-    
-    func dropFirstSafe(array:[Any]) -> [Any] {
-        if array.count <= 1 {
-            return []
-        }
-        return Array(array.dropFirst())
     }
 }
 
 Solution().isMatch("aa", "a")
 Solution().isMatch("aa", "aa")
-//Solution().isMatch("aaa", "aa")
-//Solution().isMatch("aa", "a*")
-//Solution().isMatch("aa", ".*")
-//Solution().isMatch("ab", ".*")
-//Solution().isMatch("aab", "c*a*b")
-//Solution().isMatch("a", "ab*")
+Solution().isMatch("aaa", "aa")
+Solution().isMatch("aa", "a*")
+Solution().isMatch("aa", ".*")
+Solution().isMatch("ab", ".*")
+Solution().isMatch("aab", "c*a*b")
+Solution().isMatch("a", "ab*")
 Solution().isMatch("bbacbcabbbbbcacabb", "aa*c*b*a*.*a*a.*.")
