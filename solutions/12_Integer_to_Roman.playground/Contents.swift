52137abd45dc79c6328ed6c813e20905200d187d
DaiYue <daiyue10@gmail.com>
2016-12-27 12:18:29 +0800

    #13 Roman to Integer
    Passed.

--- solutions/12_Integer_to_Roman.playground/Contents.swift
+++ solutions/12_Integer_to_Roman.playground/Contents.swift
@@ -1,5 +1,7 @@
 // #12 Integer to Roman https://leetcode.com/problems/integer-to-roman/
 // 很简单的递归，没什么可说的。就是细心一点吧。
+// 看到题解是把 1000、900、500 都存起来，这样确实快很多，因为不用考虑往左加的情况。另外非递归因为不用算 10 次幂可能略快一点。
+// 时间复杂度：O(lgn) 空间复杂度：O(1)
 
 class Solution {
     func intToRoman(_ num: Int) -> String {
---
+++ solutions/13_Roman_to_Integer.playground/Contents.swift
@@ -0,0 +1,37 @@
+// #12 Roman to Integer https://leetcode.com/problems/roman-to-integer/
+//
+
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+        let chars = [Character](s.characters)
+        guard chars.count > 0 else {
+            return 0
+        }
+        let num1 = romanLetterToInt(chars[0])
+        guard chars.count > 1 else {
+            return num1
+        }
+        let num2 = romanLetterToInt(chars[1])
+        var result = 0
+        if num1 < num2 {
+            result = num2 - num1 + romanToInt(String(chars.suffix(from: 2)))
+        } else {
+            var i = 1
+            while i < chars.count && chars[i] == chars[0] {
+                i += 1
+            }
+            result = num1 * i + romanToInt(String(chars.suffix(from: i)))
+        }
+        return result
+    }
+    
+    func romanLetterToInt(_ char: Character) -> Int {
+        let letterNumDict : [Character : Int] = ["I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000]
+        return letterNumDict[char] != nil ? letterNumDict[char]! : 0
+    }
+}
+
+Solution().romanToInt("XLIV")
+Solution().romanToInt("III")
+Solution().romanToInt("XXXI")
+Solution().romanToInt("XI")
