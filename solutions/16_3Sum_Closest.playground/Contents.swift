285a53f32d31a5a145c39660f796f0d406db86e4
DaiYue <daiyue10@gmail.com>
2016-12-29 12:15:14 +0800

    #16 3Sum closest
    Refine code & add comments.

--- solutions/16_3Sum_Closest.playground/Contents.swift
+++ solutions/16_3Sum_Closest.playground/Contents.swift
@@ -1,15 +1,15 @@
 // #16 3Sum Closest https://leetcode.com/problems/3sum-closest/
-//
+// 这时候就用上上一题的排序思路了。先排好序，然后指定第一个数从左往右，第二个为第一个数右边（剩下区间的最左端），第三个数为最右端。然后小了把左边的往右挪、大了把右边的往左挪……
+// 时间复杂度：O(n^2) 空间复杂度：O(n)
 
 class Solution {
     func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
-        guard nums.count > 1 else {
-            return nums.count == 0 ? 0 : nums[0]
+        guard nums.count >= 3 else {
+            return 0
         }
         
         var sortedNums = nums.sorted(by:{$0 < $1})
         var minDifference = Int.max
-        var closestSum = 0
         
         for i in 0 ... sortedNums.count - 3 {
             if i > 0 && sortedNums[i] == sortedNums[i - 1] {
@@ -20,12 +20,11 @@ class Solution {
             var right = sortedNums.count - 1
             while left < right {
                 let sum = sortedNums[i] + sortedNums[left] + sortedNums[right]
-                let difference = abs(target - sum)
-                if difference < minDifference {
+                let difference = target - sum
+                if abs(difference) < abs(minDifference) {
                     minDifference = difference
-                    closestSum = sum
                     if difference == 0 {
-                        break
+                        return target
                     }
                 }
                 
@@ -41,7 +40,7 @@ class Solution {
             }
         }
         
-        return closestSum
+        return target - minDifference
     }
 }
 
