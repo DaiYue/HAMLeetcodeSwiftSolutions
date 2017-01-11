a38ba0834e3261e0ad779a73f14436adbfc4f2cf
DaiYue <daiyue10@gmail.com>
2017-01-11 20:30:43 +0800

    #19 Remove Nth Node From End of List
    Refine code & add comments.

--- solutions/19_Remove_Nth_Node_From_End_of_List.playground/Contents.swift
+++ solutions/19_Remove_Nth_Node_From_End_of_List.playground/Contents.swift
@@ -1,3 +1,6 @@
+// #19 Remove Nth Node From End of List https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// 挺简单的题，没什么可说的。只要两个指针，第一个指向头部，第二个指向第 n 个节点；然后把两个指针同时往后挪，当第二个指针到尾部时，第一个指针指向的就是就是倒数第 n 个节点，把它去了就行了。
+// 时间复杂度：O(n) 空间复杂度：O(n)
 
 public class ListNode {
     public var val: Int
@@ -11,13 +14,9 @@ public class ListNode {
 class Solution {
     func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
         var length = 1
-        var queueHead = head, queueTail = head
-        var currentNode = head?.next
+        var queueHead = head, currentNode = head?.next
         
         while currentNode != nil {
-            queueTail?.next = currentNode
-            queueTail = currentNode
-            
             if length >= n + 1 {
                 queueHead = queueHead?.next
             } else {
@@ -27,12 +26,10 @@ class Solution {
             currentNode = currentNode?.next
         }
         
-        if queueHead === head && length < n + 1 {
-            if length == n {
-                return head?.next
-            } else {
-                return head
-            }
+        if length < n {
+            return head
+        } else if length == n {
+            return head?.next
         } else {
             queueHead?.next = queueHead?.next?.next
             return head
