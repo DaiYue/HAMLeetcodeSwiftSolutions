88d4e8e0a92417a31e65e877b1292fafa0c86c1d
DaiYue <daiyue10@gmail.com>
2017-02-10 17:41:40 +0800

    #30 Substring with Concatenation of All Words
    Finished.

---
+++ solutions/30_Substring_with_Concatenation_of_All_Words.playground/Contents.swift
@@ -0,0 +1,72 @@
+// #30 Substring with Concatenation of All Words
+// 呼…… 这是我写过代码最长的一题吧！但并不难，只是较为繁琐。
+// 首先把原串按单词长度划成数个子串。因为答案可能从原串的任意地方开始，所以每次循环从第 0...wordLength - 1 个字母开始，就能覆盖到所有情况。每个子串匹配到的单词存到 matchedWords 里。
+// 剩下的问题就是判断何时连续出现所有单词了…… 下面用到的方法虽然写起来很繁琐，但效率是 O(n) 的，就是一个窗口逐渐右移，两个指针分别指向两头。窗口长度不够时，右指针往右挪；遇到重复元素，左指针往右到重复元素的右边；遇到 nil，窗口跳到它的右边继续。
+// 时间复杂度：O(nm) 空间复杂度：O(n)
+
+class Solution {
+    func findSubstring(_ s: String, _ words: [String]) -> [Int] {
+        let wordLength = words[0].characters.count
+        let sChars = [Character](s.characters)
+        
+        var result = [Int]()
+        for startIndex in 0...wordLength - 1 {
+            var targetWordDict = [String:Int]()
+            for word in words {
+                targetWordDict[word] = targetWordDict[word] == nil ? 1 : targetWordDict[word]! + 1
+            }
+            
+            var matchedWords = [String?]()
+            for i in stride(from: startIndex, through: sChars.count - wordLength, by: wordLength) {
+                let word = String(sChars[i ..< i + wordLength])
+                sChars.count - wordLength
+                matchedWords.append(targetWordDict[word] == nil ? nil : word)
+            }
+            
+            var leftPointer = 0, rightPointer = 0, currentWordCount = 0
+            var wordDict = targetWordDict
+            while leftPointer < matchedWords.count && rightPointer < matchedWords.count {
+                guard let word = matchedWords[rightPointer] else {
+                    rightPointer += 1 // jump over nil
+                    leftPointer = rightPointer
+                        
+                    wordDict = targetWordDict
+                    currentWordCount = 0
+                    continue
+                }
+                    
+                if wordDict[word]! == 0 {
+                    // already has word
+                    while leftPointer < rightPointer {
+                        let wordToRemove = matchedWords[leftPointer]!
+                        leftPointer += 1
+                        if wordToRemove == word {
+                            break
+                        } else {
+                            currentWordCount -= 1
+                            wordDict[wordToRemove] = wordDict[wordToRemove]! + 1
+                        }
+                    }
+                } else {
+                    // doesn't have word
+                    wordDict[word] = wordDict[word]! - 1
+                    currentWordCount += 1
+                }
+                rightPointer += 1
+                
+                if currentWordCount == words.count {
+                    result.append(startIndex + leftPointer * wordLength)
+                    
+                    let wordToRemove = matchedWords[leftPointer]!
+                    wordDict[wordToRemove] = wordDict[wordToRemove]! + 1
+                    leftPointer += 1
+                    currentWordCount -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+}
+
+Solution().findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"])
