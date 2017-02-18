09cd0fbf093008bf274a85a9a5bef3ac97d302f5
DaiYue <daiyue10@gmail.com>
2017-02-18 18:00:02 +0800

    #37 Sudoku Solver

---
+++ solutions/37_Sudoku_Solver.playground/Contents.swift
@@ -0,0 +1,8 @@
+// #37 Sudoku Solver https://leetcode.com/problems/sudoku-solver/
+//
+
+class Solution {
+    func solveSudoku(_ board: inout [[Character]]) {
+        var possibleNums = [[[]]]
+    }
+}
